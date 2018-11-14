ed6b56ffe97b9be987a561993bac551ed1d38d5d
MisakiFx <1761607418@qq.com>
2018-11-14 20:35:42 +0800

    change something..

--- C-DS/单链表/单链表/源.cpp
+++ C-DS/单链表/单链表/源.cpp
@@ -1,5 +1,6 @@
 ﻿#include <stdio.h>
-#include "DataElement.h"
+#include <string.h>
+#include "LinkList.h"
 /*!
  * \项目名 单向链表示例
  * \日期 2018/11/13 1:31
@@ -10,7 +11,21 @@
  * 文件描述: 链表抽象数据类型的定义，每个链表都有后继节点，优点是存储的高效
  *
 */
+ElementType dataArray[] = {
+	{1, "Misaki"},
+	{2, "MisakiFx"},
+	{3, "Misakifx"},
+	{4, "MisakiFX"},
+};
+void TestLinkList()
+{
+	LinkList linkList;
+	linkList.length = 0;
+	InitLinkList(&linkList, dataArray, sizeof(dataArray) / sizeof(dataArray[0]));
+	PrintLinkList(&linkList);
+}
 int main()
 {
-
+	TestLinkList();
+	system("pause");
 }
