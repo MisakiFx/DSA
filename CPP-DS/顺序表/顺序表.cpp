a4e057b6853414467917c2559b3b121e47c7ffcb
Misaki <1761607418@qq.com>
2019-10-18 10:09:43 +0800

    2019.10.18

--- CPP-DS/顺序表/顺序表.cpp
+++ CPP-DS/顺序表/顺序表.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <Windows.h>
 using namespace std;
 #define N 100
 //顺序表的实现
@@ -86,17 +88,59 @@ public:
     //删除pos当前位置数据
     void Erase(size_t pos)
     {
-
+        //pos不合法
+        if(pos >= _size || pos < 0)
+        {
+            return;
+        }
+        for(size_t i = pos; i < _size - 1; i++)
+        {
+            _array[pos] = _array[pos + 1];
+        }
+        _size--;
     }
     //查找
     size_t Find(const T& value)
     {
-        
+        for(size_t i = 0; i < _size; i++)
+        {
+            if(_array[i] == value)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    //二分查找
+    size_t BinaryFind(const T& value)
+    {
+        //左闭右开区间
+        size_t high = _size, low = 0;
+        while(high > low)
+        {
+            size_t mid = (high + low) / 2;
+            if(_array[mid] == value)
+            {
+                return mid;
+            }
+            else if(_array[mid] > value)
+            {
+                high = mid;    
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
     }
     //修改
     void Modify(size_t pos, const T& value)
     {
-
+        if(pos < 0 || pos >= _size)
+        {
+            return;
+        }
+        _array[pos] = value;
     }
     //打印
     void Print()
@@ -111,12 +155,33 @@ public:
     //当前元素个数
     size_t Size()
     {
-
+        return _size;
     }
     //某个位置的值
     T& operator[](size_t pos)
     {
-
+        assert(pos >=0 && pos < _size);
+        return _array[pos];
+    }
+    //冒泡排序
+    void BubbleSort()
+    {
+        for(int i = 0; i < _size - 1; i++)
+        {
+            bool flag = false;
+            for(int j = 0; j < _size - i - 1; j++)
+            {
+                if(_array[j] > _array[j + 1])
+                {
+                    swap(_array[j], _array[j + 1]);
+                    flag = true;
+                }
+            }
+            if(flag == false)
+            {
+                break;
+            }
+        }
     }
 private:
     //T _array[N];//静态顺序表，利用数组，不可变，十分不灵活
@@ -143,11 +208,15 @@ int main()
     SeqList<int> list;
     list.Push_back(9);
     list.Push_back(1);
-    list.Push_back(2);
-    //list.Pop_back();
-    //list.Pop_back();
+    list.Push_back(10);
+    list.Push_back(11);
+    list.Push_back(20);
+    list.Push_back(0);
+    list.Push_back(-1);
     list.Push_front(1);
     list.Print();
-    list.Insert(4 , 12);
+    list.BubbleSort();
     list.Print();
+    int pos = list.BinaryFind(1);
+    std::cout << pos << std::endl;
 }
